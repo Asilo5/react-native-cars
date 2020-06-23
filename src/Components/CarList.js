93da78b3ac88911e2e4e39be6b28e741a2eb8c46
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-23 11:24:43 -0600

    Add import of CarDetail to CarList to prop drill down the information

--- src/Components/CarList.js
+++ src/Components/CarList.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import axios from 'axios';
+
+import CarDetail from './CarDetail';
 
 class CarList extends Component {
   state = {
@@ -15,7 +17,7 @@ class CarList extends Component {
 
   renderList = () => {
     return this.state.carList.map(car => {
-      return <Text key={car.brand}>{car.model[0].name}</Text>;
+      return <CarDetail brand={car} key={car.model} />;
     });
   };
 
