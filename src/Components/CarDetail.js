fc8c03d9364d09e2017dcd7a2613d7b270f83ddd
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-23 11:17:14 -0600

    Add basic layout of CarDetail component

---
+++ src/Components/CarDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CarDetail = () => {
+    return (
+      <View>
+          
+      </View>
+    );
+}
+
+export default CarDetail;
