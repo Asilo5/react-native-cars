6ef18b4cde583cceb2b4bac83f6b4935e1e8fae7
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-22 20:33:02 -0600

    Install axios package to fetch data

--- src/Components/CarList.js
+++ src/Components/CarList.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
 class CarList extends Component {
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <View>
