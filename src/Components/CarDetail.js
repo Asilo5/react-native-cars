7345197162ca70fd12c1dae383bf6087f4b6d6c1
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-23 12:07:32 -0600

    Add styles to the carDetail component

--- src/Components/CarDetail.js
+++ src/Components/CarDetail.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, Button, StyleSheet} from 'react-native';
 
 const CarDetail = ({brand}) => {
   return (
-    <View>
-      <Text>{brand.model[0].name}</Text>
+    <View style={styles.viewStyle}>
+      <View>
+        <Text>{brand.brand}</Text>
+      </View>
+      {brand.model.map(models => {
+        return (
+          <View>
+            <Text>{models.name}</Text>
+            {/* <Image source={brand.model[0].image} /> */}
+          </View>
+        );
+      })}
+      <View>
+        <Text>Go To Site</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 1,
+    borderColor: 'gainsboro',
+    shadowColor: 'black',
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 0.1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+  },
+});
 
 export default CarDetail;
