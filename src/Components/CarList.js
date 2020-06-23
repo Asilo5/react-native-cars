be8579bd7613a821a1ddc163eeb6545d6b176c0d
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-22 20:54:11 -0600

    Set state to fetched url

--- src/Components/CarList.js
+++ src/Components/CarList.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import axios from 'axios';
 
 class CarList extends Component {
-  componentDidMount() {
+  state = {
+    carList: [],
+  };
 
+  componentDidMount() {
+    axios
+      .get('http://givecars.herokuapp.com/')
+      .then(resp => this.setState({carList: resp}));
   }
 
   render() {
