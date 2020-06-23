a0c519813b024dfaaaf7e714ceff71b84ab40abb
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-22 18:36:52 -0600

    Add Header file and move App into Components file

---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const Header = () => {
+  return (
+    <View>
+      <Text> Cars </Text>
+    </View>
+  );
+};
+
+// const styles = StyleSheet.create({
+
+// });
+
+export default Header;
